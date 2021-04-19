bc24520d654f8c4df90d9d3da7367483700fb375
Zero Meia <59213109+ZeroMe1a@users.noreply.github.com>
2021-04-19 03:51:49 -0400

    Revert "Revert "Adicionar arquivos de projeto.""
    This reverts commit 15c3ecf80475e60ccfd861f7d9b6ef6ff826170c.

---
+++ cPoligny/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace cPoligny
+{
+    class Program
+    {
+        static int ang_s(int s)
+        {
+            return (s - 2) * 180;
+        }
+
+        static int sid_a(int a)
+        {
+            return (a / 180) + 2;
+        }
+
+        static void Main()
+        {
+            Console.Title = "cPoligny Testing";
+
+            Console.WriteLine("Welcome to cPoligny!" +
+                "\n\n[0] Quit" +
+                "\n[1] Internal Angle using Sides" +
+                "\n[2] Sides using Internal Angle\n");
+
+
+            Console.Write("?: ");
+            int ch = Convert.ToInt32(Console.ReadLine());
+
+
+            switch (ch)
+            {
+                case 0:
+                    Environment.Exit(0);
+                    break;
+
+                case 1:
+                    Console.Write("\nN° Sides: ");
+                    int x = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine($"\nAnswer: {ang_s(x)}");
+
+                    Console.Write("Press any key to quit...");
+                    Console.ReadKey();
+
+                    break;
+
+                case 2:
+                    Console.Write("N° Sides: ");
+                    x = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine($"\nAnswer: {sid_a(x)}");
+
+                    Console.Write("Press any key to quit...");
+                    Console.ReadKey();
+
+                    break;
+
+                default:
+                    Console.WriteLine("Unrecognized");
+
+                    Console.Write("Press any key to return...");
+                    Console.ReadKey();
+
+                    Main();
+                    break;
+
+            } 
+        }
+    }
+}
